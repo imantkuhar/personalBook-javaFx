40094480b8e593adeb625469e32da278e244676d
Imant <imantkuhar93@gmail.com>
2016-11-18 18:23:38 +0200

    modify ui

--- src/main/java/controllers/AddContactController.java
+++ src/main/java/controllers/AddContactController.java
@@ -1,7 +1,17 @@
 package controllers;
 
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Created by Imant on 17.11.16.
  */
-public class AddContactController {
+public class AddContactController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
--- src/main/java/controllers/EditContactController.java
+++ src/main/java/controllers/EditContactController.java
@@ -1,7 +1,17 @@
 package controllers;
 
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Created by Imant on 17.11.16.
  */
-public class EditContactController {
+public class EditContactController  implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
